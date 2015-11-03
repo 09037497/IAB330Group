3e8f95826e19ae83a800f7d4308c75f02df7bde0
carmanyeung <carmanyeung512@gmail.com>
2015-11-03 15:00:37 +1000

    UI with database v1

---
+++ Todo-2/SharedProject/Todo/App.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Todo
+{
+	public class App : Application
+	{
+		static TodoItemDatabase database;
+		public static TodoItemDatabase Database {
+			get {
+				database = database ?? new TodoItemDatabase ();
+				return database;
+			}
+		}
+
+		public App()
+		{
+			//MainPage = new NavigationPage (new TodoListPage ());
+			MainPage = new NavigationPage (new FirstPage());
+		}
+	}
+}
